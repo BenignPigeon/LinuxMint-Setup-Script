26885475361cbef9897718c1475b6753292c00f1
BenignPigeon <BenignPigeon@users.noreply.github.com>
2026-01-19 16:38:36 +0000

    misc patches

--- bin/misc-patches.sh
+++ bin/misc-patches.sh
@@ -1,2 +1,6 @@
 # File manager to show hidden files by default
 gsettings set org.nemo.preferences show-hidden-files true
+
+# Show home and recycling on desktop
+gsettings set org.nemo.desktop home-icon-visible true
+gsettings set org.nemo.desktop trash-icon-visible true
